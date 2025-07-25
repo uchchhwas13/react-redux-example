8527bfcf70fe02501bea485aa6005498ce1409e5
Uchchhwas Roy <uchchhwas.roy.du@gmail.com>
2025-07-25 11:18:39 +0600

    Passing handleIncrement, handleDecrement props to Counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,38 @@
+import { useState } from "react";
 import Counter from "./Counter";
 import TotalCounter from "./TotalCounter"
+
+const initialCounters = [
+  {
+    id:1,
+    value: 0
+  },
+  {
+    id:2,
+    value: 0
+  }
+]
 function App() {
+  const [counters, setCounter] = useState(initialCounters);
+  
+  const handleIncrement = (counterId) => {
+
+  };
+
+  const handleDecrement = (counterId) => {
+
+  };
+   
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100 p-10">
       <div className="flex gap-8 flex-wrap justify-center">
-        <Counter label="Counter One" />
-        <Counter label="Counter Two" />
+        {
+          counters.map((counter) => (
+            <Counter key={counter.id} label={"Counter " + counter.id} onIncrement = {handleIncrement} 
+                      onDecrement={handleDecrement}/>
+          ))
+        }
+
         <TotalCounter total={10} />
       </div>
     </div>
--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,7 +1,7 @@
 // src/components/Counter.jsx
 import { useState } from "react";
 
-export default function Counter({ label }) {
+export default function Counter({ label, onIncrement, onDecrement }) {
   const [count, setCount] = useState(0);
 
   return (
@@ -10,13 +10,13 @@ export default function Counter({ label }) {
       <p className="text-3xl font-bold mb-4">{count}</p>
       <div className="flex justify-center gap-4">
         <button
-          onClick={() => setCount(count - 1)}
+          onClick={onDecrement}
           className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600"
         >
           -
         </button>
         <button
-          onClick={() => setCount(count + 1)}
+          onClick={onIncrement}
           className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600"
         >
           +
